57ac470ee87f19c626d410017dcbcf8f1ad55212
geraldy1st <geraldy.leondas@gmail.com>
2021-01-05 23:07:20 +0100

    navbar setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 
 
-function App() {
-  return (
-        <div className="App">
-            <Navbar />
-        </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar title={"Testing Mode"} />
+                <h1>Elo</h1>
+            </div>);
+    }
 }
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Navbar.style.css';
 import logo from '../images/vsfe5.png';
+import PropTypes from 'prop-types';
 
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <img src={logo} className="logo" alt="logo" />
-      <ul>
-        <li>
-          <a href="./Home">Home</a>
-        </li>
-        <li>
-          <a href="./About">About</a>
-        </li>
-      </ul>
-    </nav>
-  );
+class Navbar extends Component {
+    static defaultProps = {
+        title: "Elo Brain App" 
+    }
+
+    static propTypes = {
+        title: PropTypes.string.isRequired
+    }
+
+    render(){
+        return (
+            <nav className="navbar bg-primary">
+                <img src={logo} className="logo" alt="logo" />
+                {this.props.title}
+                <ul>
+                    <li>
+                    <a href="./Home">Home</a>
+                    </li>
+                    <li>
+                    <a href="./About">About</a>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
 };
 
 export default Navbar;
