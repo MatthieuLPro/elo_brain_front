2346711f73717709bbbaf73b53cbba2396775a7c
geraldy1st <geraldy.leondas@gmail.com>
2021-01-06 23:22:55 +0100

    modif navbar, de class component en fonctionnal component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Navbar.style.css';
 import logo from '../images/vsfe5.png';
 import PropTypes from 'prop-types';
 
-class Navbar extends Component {
-    static defaultProps = {
-        title: "Elo Brain App" 
-    }
-
-    static propTypes = {
-        title: PropTypes.string.isRequired
-    }
-
-    render(){
-        return (
-            <nav className="navbar bg-primary">
-                <img src={logo} className="logo" alt="logo" />
-                {this.props.title}
-                <ul>
-                    <li>
-                    <a href="./Home">Home</a>
-                    </li>
-                    <li>
-                    <a href="./About">About</a>
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
+const Navbar = (props) => {
+    return (
+        <nav className="navbar bg-primary">
+            <img src={logo} className="logo" alt="logo" />
+            {props.title}
+            <ul>
+                <li>
+                <a href="./Home">Home</a>
+                </li>
+                <li>
+                <a href="./About">About</a>
+                </li>
+            </ul>
+        </nav>
+    );   
 };
+
+Navbar.defaultProps = {
+    title: "Elo Brain App" 
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired
+}
 
 export default Navbar;
--- src/components/users/UserInfo.js
+++ src/components/users/UserInfo.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class UserInfo extends Component {
-
-  render() {
-        const {username,team_name} = this.props.user;
-
-        return (
+const UserInfo = (props) =>  {
+    const {username,team_name} = props.user;
+    
+    return (
             <div className="card text-center">
                 {/* <img src={avatar_url} alt="" className="round-img" style={{width:"60px"}} /> */}
                 <h2>{username}</h2>
@@ -15,7 +13,8 @@ class UserInfo extends Component {
                 </div>
             </div>
         )
-    }
 }
+
+
 
 export default UserInfo;
